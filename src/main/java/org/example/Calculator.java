b399e6f84b1149ee781d7d50a2a187dbb23089b1
BlueRasputin <barrettjoshrev10@gmail.com>
2025-06-08 21:49:24 -0500

    Still broken but need to look at solutions to find and fix errors

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,11 +1,54 @@
 package org.example;
+import java.util.Scanner;
 
 public class Calculator {
-    // Add a static function called add that takes two int parameters and returns their sum (as an int)
+    public class Main {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the first number: ");
+            int a = scanner.nextInt();
+            System.out.print("Enter the second number: ");
+            int b = scanner.nextInt();
+            System.out.println("The sum is " + add(a, b));
+            System.out.println("The difference is " + subtract(a, b));
+            System.out.println("The product is " + multiply(a, b));
+            System.out.println("The quotient is " + divide(a, b));
+        }
 
-    // Add a static function called subtract that takes two int parameters and returns their difference (as an int)
+
+        // Add a static function called add that takes two int parameters and returns their sum (as an int)
+
+        public static int add( int a, int b ) {
+
+            return a + b;
+
+        }
+
+        // Add a static function called subtract that takes two int parameters and returns their difference (as an int)
+
+        public static int subtract( int a, int b ) {
+
+            return a - b;
+
+        }
+
+
 
     // Add a static function called multiply that takes two int parameters and returns their product (as an int)
 
+        public static int multiply( int a, int b ) {
+
+            return a * b;
+
+        }
+
+
+
     // Add a static function called divide that takes two int parameters and returns their quotient (as an int)
+
+        public static int divide( int a, int b) {
+            return a / b;
+        }
+
+    }
 }
--- src/main/java/org/example/StringUtilities.java
+++ src/main/java/org/example/StringUtilities.java
@@ -1,16 +1,64 @@
 package org.example;
-
+import java.util.Scanner;
 public class StringUtilities {
     // Refer to Java Basics 1 for common string methods
 
     // Add a static function called shortString that takes a String parameter and returns true if the string has fewer than 5
     // characters
+    public static void shortString(String args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str1 = scanner.nextLine();
+        int str1l = str1.length;
+        if (str1l < 5) {
+            System.out.println("String is less than 5 characters");
+        } else {
+            System.out.println("String is at least 5 characters");
+
+        }
+    }
+
+
 
     // Add a static function called firstLetter that takes a String parameter and returns the first character of the string
+    public static char firstLetter(String args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str2 = scanner.nextLine();
+        result = str2.charAt(1);
+        return result;
+    }
+
+
+
 
     // Add a static function called censorAsparagus that takes a String parameter and returns the string with all instances of
     // "asparagus" (lowercase only) replaced with 4 stars: "****"
+    public static void censorAsparagus(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str3 = scanner.nextLine();
+        if (str3.contains("asparagus")) {
+            return str3.replace("asparagus", "****");
+        };
+
+    }
+
+
+
+
 
     // Add a static function called bigger that takes two String parameters and returns the longer of the two strings. If the
     // strings are the same length, return the first string.
+
+    public static void bigger(String str3, String str4) {
+        if (str4.length() > str3.length()) {
+            return str4;
+        } else {
+            return str3;
+        }
+    }
+
+
+
 }
